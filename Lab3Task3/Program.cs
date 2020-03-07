c532d503055cbcd79e31817bd0b06346d2bf82de
KiptenkoBogdan <60790019+KiptenkoBogdan@users.noreply.github.com>
2020-03-07 12:07:03 +0200

    Add files via upload

---
+++ Lab3Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lab3Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            char[] separator = { ' ', '.', '*', '+', ';', ',', '?', '!', '‐', '/' };
+            StringBuilder result = new StringBuilder();
+            using (StreamReader File = new StreamReader(@"C:\Users\User\Desktop\C#Programming\Lab3Task3\text.txt"))
+            {
+                if (File == null)
+                    Console.WriteLine("File missing!");
+                else
+                {
+                    while ((line = File.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        string[] words = line.Split(separator);
+                        foreach (string word in words)
+                        {
+                            if ((word.Length >= 2))
+                            {
+                                result.Append(word[0]);
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Result: ");
+            Console.WriteLine(result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
